5e540e0c85d4225b5249e7e2b3f81535e744a2c5
ashishkumbhar01 <ashishkumbhar01@hotmail.com>
2024-03-30 13:42:36 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+define("APP_ROOT", __DIR__);
+
+require_once APP_ROOT . "/vendor/autoload.php";
+
+spl_autoload_register(function($class)
+{
+  $classFile = str_replace("\\", DIRECTORY_SEPARATOR, $class .".php");
+  $classPath = APP_ROOT ."/app/". $classFile;
+  if (file_exists($classPath))
+  {
+    require_once $classPath;
+  }
+});
+
+session_start();
+
+use App\Services\Route;
+
+$route = new Route();
+require_once APP_ROOT ."/routes/route.php";
+$route->handle();
