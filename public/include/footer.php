2951f1465a85f1ba8876900874ee3ed6f2b78b29
ashishkumbhar01 <ashishkumbhar01@hotmail.com>
2024-12-19 19:15:50 +0530

    update

--- public/include/footer.php
+++ public/include/footer.php
@@ -1,6 +1,7 @@
     
-    <script src="/resources/js/bootstrap.bundle.min.js"></script>
-    <script src="/resources/js/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js"></script>
     <script src="/resources/js/main.js"></script>
   </body>
 </html>
--- public/include/header.php
+++ public/include/header.php
@@ -4,7 +4,7 @@
     <title>PHP Chat App</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
 
